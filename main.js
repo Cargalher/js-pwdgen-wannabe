0e366b9fc63ae0248b8c8b0165e3505e3b96aa68
Cargalher <cargalher@gmail.com>
2021-04-22 15:36:26 +0200

    "safe" password created

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+//ask user's name
+var userName = prompt ('What is your name?');
+
+//ask user's last name
+var lastName = prompt ('What is your last name?');
+
+//ask user's favorite color
+var favColor = prompt ('What is your favorite color?');
+
+//show a password message
+var passwordElement = document.getElementById('password');
+passwordElement.innerHTML = "Password";
+
+//show created password
+var secret_passwordElement = document.getElementById ('secret_password');
+secret_passwordElement.innerHTML = userName + lastName + favColor + '21';
 
 
